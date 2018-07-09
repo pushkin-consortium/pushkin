11206f7bffb6b2bad45a6b98820f0821ff618b9e
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-07-09 14:30:30 -0400

    added more react support

--- front-end/.eslintrc.js
+++ front-end/.eslintrc.js
@@ -1,37 +1,40 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es6": true,
+		"node": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"react/recommended"
+	],
+	"parserOptions": {
+		"ecmaFeatures": {
+			"experimentalObjectRestSpread": true,
+			"jsx": true
+		},
+		"sourceType": "module"
+	},
+	"plugins": [
+		"react"
+	],
+	"rules": {
+		"indent": [
+			1,
+			"tab"
+		],
+		"linebreak-style": [
+			1,
+			"unix"
+		],
+		"quotes": [
+			1,
+			"single"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
 };
