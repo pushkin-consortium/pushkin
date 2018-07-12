deab748c33b5249747c754dba5a9c16379328450
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-07-12 14:39:27 -0400

    sed was wrong

--- server/start.sh
+++ server/start.sh
@@ -4,7 +4,9 @@
 # could just replace everything through a envsubst, possibly,
 # rather than using sed
 echo "adding api port to config: ${API_PORT}"
-sed 's/${API_PORT}/'"$API_PORT"'/g' < nginx.conf > nginx.conf
+# set to 3000 by default
+API_PORT=${API_PORT:-3000}
+sed -i 's/${API_PORT}/'"$API_PORT"'/g' nginx.conf
 
 # nginx container's default start command
 nginx -g "daemon off;"
