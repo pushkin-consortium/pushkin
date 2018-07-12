ef182534cfbae8c3de0bb1542f2a616d32c7409b
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-07-12 10:31:07 -0400

    simplifying

--- api/server.js
+++ api/server.js
@@ -24,15 +24,17 @@ amqp.connect(process.env.AMQP_ADDRESS, function(err, conn) {
     next();
   });
 
+	// include custom quiz api controllers
 	fs.readdirSync(path.resolve(__dirname, 'controllers'))
 		.filter(file => path.parse(file).ext === '.js')
-		.forEch(controllerFile => {
+		.forEach(controllerFile => {
 			const short = controllerFile.replace('.js', '');
 			const route = '/api/' + short;
 			const controller = require('./controllers/' + short)(rpc, conn, dbWrite);
 			app.use(route, controller);
 		});
 
+	// main routes/quiz independent
   if (CONFIG.forum) {
     const forumController = require('./forum')(rpc, conn, dbWrite);
     app.use('/api', forumController);
@@ -41,6 +43,7 @@ amqp.connect(process.env.AMQP_ADDRESS, function(err, conn) {
     const authController = require('./auth')(rpc, conn, dbWrite);
     app.use('/api', authController);
   }
+
   app.get('/api/users', (req, res, next) => {
     var rpcInput = {
       method: 'allUsers'
@@ -52,6 +55,7 @@ amqp.connect(process.env.AMQP_ADDRESS, function(err, conn) {
       })
       .catch(next);
   });
+
   app.get('/api/mail/health', (req, res, next) => {
     var rpcInput = {
       method: 'health'
@@ -68,18 +72,22 @@ amqp.connect(process.env.AMQP_ADDRESS, function(err, conn) {
     const routes = printer(app);
     res.send(routes);
   });
+
   app.use((req, res, next) => {
     res.status(404).json({ message: 'Route not found' });
   });
+
   app.use((err, req, res, next) => {
     res.status(500);
     res.json(err);
     logger.error(err.message);
   });
+
   app.listen(PORT, function() {
     // require('express-routemap')(app)
     //Callback triggered when server is successfully listening. Hurray!
     console.log('Server listening on: http://localhost:%s', PORT);
   });
 });
+
 module.exports = app;
