e24e242717750524c35d0dcc21ebd955aba7dc62
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-07-16 12:59:35 -0400

    readded quizzes folder why do you keep disapparing

---
+++ quizzes/quizzes/newquiz2/quiz_page/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './styles.scss'
+
+export default class newquiz2 extends React.Component {
+	render() {
+		return (
+			<p>Welcome to the newquiz2 quiz!</p>
+		);
+	}
+}
---
+++ quizzes/quizzes/testquiz/quiz_page/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './styles.scss'
+
+export default class testquiz extends React.Component {
+	render() {
+		return (
+			<p>Welcome to the testquiz quiz!</p>
+		);
+	}
+}
