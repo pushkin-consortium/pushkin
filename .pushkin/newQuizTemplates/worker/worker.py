6a68d3e0ce1045eba191a7602c3b895267a61a70
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-07-18 13:04:55 -0400

    two queues, methods correspond with api (at least the basic ones)

--- .pushkin/newQuizTemplates/worker/worker.py
+++ .pushkin/newQuizTemplates/worker/worker.py
@@ -3,48 +3,55 @@ import os
 import json
 import handleResponse
 
-
 def consumeCallback(ch, method, props, body):
-    bodyJSON = json.loads(body)
+    bodyJson = json.loads(body)
+    # body is expected to have these from the api controller
     rpcParamMethod = bodyJSON['method']
     rpcParamData = bodyJSON['data']
 
-    print('method: {}\ndata: {}'.format(rpcParamMethod, rpcParamData))
-
     result = handler.handle(rpcParamMethod, rpcParamData)
-    responseJSON = json.dumps(result)
-
-    print('response: {}'.format(responseJSON))
+    responseJson = json.dumps(result)
 
     ch.basic_publish(exchange = '',
             routing_key = props.reply_to,
             properties = pika.BasicProperties(correlation_id=props.correlation_id),
-            body = responseJSON
+            body = responseJson
             )
 
     ch.basic_ack(delivery_tag = method.delivery_tag)
+
+def on_channel_open(ch):
+    ch.basic_qos(prefetch_count=1)
+
+    ch.queue_declare(queue=readQueue, durable=False)
+    ch.queue_declare(queue=writeQueue, durable=True)
+
+    ch.basic_consume(consumeCallback, queue=readQueue)
+    ch.basic_consume(consumeCallback, queue=writeQueue)
+
+def on_open(con):
+    con.channel(on_channel_open)
+
 ###############################
-# start consuming
+# setup
 ###############################
 
-listenOnQueue = '${QUIZ_NAME}_api_queue'
+readQueue = '${QUIZ_NAME}_quiz_db_read'
+writeQueue = '${QUIZ_NAME}_quiz_db_write'
+
 rabbitAddress = os.environ['AMQP_ADDRESS']
 mainDbUrl = os.environ['DATABASE_URL']
 transDbUrl = os.environ['TRANSACTION_DATABASE_URL']
 
-connParams = pika.URLParameters(rabbitAddress)
-connection = pika.BlockingConnection(connParams)
-channel = connection.channel()
-
-channel.queue_declare(queue=listenOnQueue, durable=True)
-channel.basic_qos(prefetch_count=1)
-
 handler = handleResponse.Handler(mainDbUrl, transDbUrl)
 
-channel.basic_consume(consumeCallback, queue=listenOnQueue)
-print('consuming')
-channel.start_consuming()
-
-
-
-
+###############################################
+# consume
+###############################################
+connParams = pika.URLParameters(rabbitAddress)
+connection = pika.SelectConnection(parameters=connParams,
+                                on_open_callback=on_open)
+try:
+    channel = connection.channel()
+except KeyboardInterrupt:
+    connection.close()
