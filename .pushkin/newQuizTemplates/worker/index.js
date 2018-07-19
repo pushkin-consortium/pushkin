7ae3e5e81f9e2b09d7b73aea93e664cfb623bbb3
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-07-19 12:05:40 -0400

    promises

--- .pushkin/newQuizTemplates/worker/index.js
+++ .pushkin/newQuizTemplates/worker/index.js
@@ -1,8 +1,8 @@
-const amqp = require('amqplib/callback_api');
-
-const AMQP_ADDRESS = process.env.AMQP_ADDRESS;
+const AMQP_ADDRESS = 'amqp://localhost'; //process.env.AMQP_ADDRESS;
 const QUEUE = '${QUIZ_NAME}_quiz_dbread';
 
+const amqp = require('amqplib/callback_api');
+const Handler = new (require('./handler'));
 amqp.connect(AMQP_ADDRESS, (err, conn) => {
 	if (err) {
 		console.log(err);
@@ -12,18 +12,25 @@ amqp.connect(AMQP_ADDRESS, (err, conn) => {
 		ch.assertQueue(QUEUE, {durable: false});
 		ch.prefetch(1);
 		ch.consume(QUEUE, msg => {
-			const data = JSON.parse(msg.content.toString());
-			console.log(`received rabbit message with data: ${data}`);
-
-			// connect to databases and do stuff
-			const r = { message: data };
-
-			ch.sendToQueue(msg.properties.replyTo,
-				new Buffer(JSON.stringify(r)),
-				{correlationId: msg.properties.correlationId}
-			);
-
-			ch.ack(msg);
+			Promise.resolve(msg.content.toString())
+				.then(JSON.parse)
+				.then(Handler.handle)
+				.then(res => {
+					if (res) {
+						ch.sendToQueue(msg.properties.replyTo,
+							new Buffer(JSON.stringify(res)),
+							{correlationId: msg.properties.correlationId}
+						);
+					}
+					ch.ack(msg);
+				})
+				.catch(err => {
+					console.log(err);
+					ch.sendToQueue(msg.properties.replyTo,
+						new Buffer(JSON.stringify(err)),
+						{correlationId: msg.properties.correlationId}
+					);
+				});
 		});
 	});
 });
