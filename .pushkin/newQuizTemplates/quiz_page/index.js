db528ef3469a860d4f1fd7690b6eab5b7e1c67a7
HanXiangC <Choongh@bc.edu>
2018-07-19 16:05:52 -0400

    Insert require statement on index.js

--- .pushkin/newQuizTemplates/quiz_page/index.js
+++ .pushkin/newQuizTemplates/quiz_page/index.js
@@ -1,11 +1,11 @@
 // At build time, this (and everything else in this folder) is moved to
 // ${pushkin_front_end_quizzes_dir}, so all paths to modules located outside of this
 // folder are relative to that root
+const jsPsych = require("../libraries/jsPsych/jspsych.js")
 
 import React from 'react'
 import s from './styles.scss'
 
-require("https://cdn.jsdelivr.net/gh/jspsych/jsPsych@6.0.4/")
 export default class ${QUIZ_NAME} extends React.Component {
 
 	render() {
