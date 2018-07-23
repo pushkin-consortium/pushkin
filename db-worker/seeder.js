291b6dfe5cbc983178ef998248529faea4d96762
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-07-23 17:10:30 -0400

    going to use knex built-in seeding

--- db-worker/seeder.js
+++ db-worker/seeder.js
@@ -3,71 +3,40 @@ const inquirer = require('inquirer');
 const chalk = require('chalk');
 
 const quizName = process.argv[2];
-const quizesDir = fs.readdirSync(__dirname + '/seeds');
+const quizSeeder = require(`${__dirname}/seeds/${quizName}/index`);
 
-const quizFolder = quizesDir.filter(folder => folder === quizName);
+inquirer.prompt([{
+	type: 'confirm',
+	name: 'inDocker',
+	message: 'Are you in pushkin-db docker?'
+}])
+	.then(answer => {
+		if (!answer.inDocker)
+			throw new Error('You must be in a docker container with the database URLs as environment variables to seed.');
 
-if (quizFolder) {
-  inquirer
-    .prompt([
-      {
-        type: 'confirm',
-        name: 'inDocker',
-        message: 'Are you in pushkin-db docker?'
-      }
-    ])
-    .then(answer => {
-      if (answer.inDocker) {
-        inquirer
-          .prompt([
-            {
-              type: 'confirm',
-              name: 'ranMigration',
-              message: 'Have you ran your migrations?'
-            }
-          ])
-          .then(answer => {
-            if (answer.ranMigration) {
-              inquirer
-                .prompt([
-                  {
-                    type: 'confirm',
-                    name: 'properSeeds',
-                    message: 'Have you properly filled out your seed CSV files?'
-                  }
-                ])
-                .then(answer => {
-                  if (answer.properSeeds) {
-                    const runSeed = require(`./seeds/${quizFolder}index`);
-                    runSeed();
-                  } else {
-                    console.log(
-                      chalk.red(
-                        'Please ensure that you have correct CSV files before running this command. For more info go to ' +
-                          chalk.blue('https://github.com/l3atbc/pushkin-db')
-                      )
-                    );
-                    process.exit();
-                  }
-                });
-            } else {
-              console.log(
-                chalk.red(
-                  'please run your migrations before running this command'
-                )
-              );
-              process.exit();
-            }
-          });
-      } else {
-        console.log(
-          chalk.red(
-            'please run this command in your pushkin-db docker container'
-          )
-        );
-        process.exit();
-      }
-    });
-} else {
-  console.log(chalk.red('quiz not found'));
-}
+		return inquirer.prompt([{
+			type: 'confirm',
+			name: 'ranMigration',
+			message: 'Have you ran your migrations?'
+		}])
+	})
+	.then(answer => {
+		if (!answer.ranMigration)
+			throw new Error('The database can\'t be seeded until the migrations have been run.');
+
+		return inquirer.prompt([{
+			type: 'confirm',
+			name: 'madeSeeds',
+			message: 'Do you have the seeds filled out as you would like them to be in the database?'
+		}])
+	})
+	.then(answer => {
+		if (!answer.madeSeeds)
+			throw new Error('Fill out the seeds in your quiz folder, run \'pushkin prep\', and try again from a rebuilt container.');
+
+		// all good
+		quizSeeder();
+	})
+	.catch(err => {
+		console.log(chalk.red(err));
+	});
