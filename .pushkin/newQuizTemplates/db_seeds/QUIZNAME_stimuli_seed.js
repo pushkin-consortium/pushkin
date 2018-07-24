4c13b27fddbfbb38885c548e6eba78bbc5a761ff
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-07-24 10:16:12 -0400

    QUIZNAME replacement in filenames, hardcoded timestamps (unecessary to have them change for the template/example quiz)

--- .pushkin/newQuizTemplates/db_seeds/QUIZNAME_stimuli_seed.js
+++ .pushkin/newQuizTemplates/db_seeds/QUIZNAME_stimuli_seed.js
@@ -1,7 +1,12 @@
-const seeder = require('knex-csv-seeder').default;
 
-exports.seed = seeder({
-  table: 'users',
-	file: '/path/to/users.csv'
-}
-
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('${QUIZ_NAME}_stimuli').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('${QUIZ_NAME}_stimuli').insert([
+				{ task: 'task1', stimulus: `What's your favorite color?`, options: `orange, green, blue`, num_responses: 0 },
+				{ task: 'task2', stimulus: `What's your favorite number?`, options: `7, 8, 9`, num_responses: 0 }
+      ]);
+    });
+};
