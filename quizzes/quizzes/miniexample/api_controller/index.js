4a1275e02758a125cf87660519a3beadf82df6d6
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-07-27 13:00:33 -0400

    wrapped response to avoid sending numbers as a status code

--- quizzes/quizzes/miniexample/api_controller/index.js
+++ quizzes/quizzes/miniexample/api_controller/index.js
@@ -10,37 +10,31 @@ module.exports = (rpc, conn) => {
 	// everything is just going to use POST as of 7/23/18's meeting
 	const stdPosts = [
 		{ path: '/response', method: 'insertResponse', queue: db_write_queue },
-
-		{ path: '/createUserWithAuth', method: 'generateUserWithAuth', queue: db_write_queue }
-
+		{ path: '/createUserWithAuth', method: 'generateUserWithAuth', queue: db_write_queue },
 		{ path: '/createUser', method: 'generateUser', queue: db_write_queue },
-
 		{ path: '/users/:auth_id', method: 'updateUser', queue: db_write_queue },
-
 		{ path: '/startExperiment', method: 'startExperiment', queue: task_queue },
-
 		{ path: '/getAllStimuli', method: 'getAllStimuli', queue: db_read_queue },
-
 		{ path: '/metaResponse', method: 'insertMetaResponse', queue: db_write_queue },
-
 		{ path: '/stimulusResponse', method: 'insertStimulusResponse', queue: db_write_queue },
-
 		{ path: '/nextStimulus', method: 'nextStimulus', queue: task_queue },
-
 		{ path: '/feedback', method: 'getFeedback', queue: task_queue },
-
 		{ path: '/activateStimuli', method: 'activateStimuli', queue: task_queue }
 	];
 
 	stdPosts.forEach(point =>
 		router.post(point.path, async (req, res, next) => {
+			console.log(`${point.path} hit`);
 			const rpcParams = {
 				method: point.method,
 				data: req.body
 			};
 			try {
 				const rpcRes = await rpc(conn, point.queue, rpcParams);
+				console.log(`got RPC res: ${rpcRes}`);
+				res.send({ apiData: rpcRes });
 			} catch (e) {
+				res.send('ERROR');
 				console.log('Error in API getting RPC response');
 				console.log(e);
 			}
