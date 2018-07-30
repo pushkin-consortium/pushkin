b47c9f39dfb71b38bb3d2fa132e0e62162035c50
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-07-30 13:30:06 -0400

    consume on task queue as well

--- quizzes/quizzes/miniexample/worker/index.js
+++ quizzes/quizzes/miniexample/worker/index.js
@@ -1,6 +1,7 @@
 const AMQP_ADDRESS = process.env.AMQP_ADDRESS || 'amqp://localhost';
 const READ_QUEUE = 'miniexample_quiz_dbread';
 const WRITE_QUEUE = 'miniexample_quiz_dbwrite';
+const TASK_QUEUE = 'miniexample_quiz_taskworker'; // eventually this should be moved to a separate thing
 
 const amqp = require('amqplib/callback_api');
 const Handler = new (require('./handler'))();
@@ -17,6 +18,7 @@ amqp.connect(AMQP_ADDRESS, (err, conn) => {
 		}
 		ch.assertQueue(READ_QUEUE, {durable: false});
 		ch.assertQueue(WRITE_QUEUE, {durable: true});
+		ch.assertQueue(TASK_QUEUE, {durable: false});
 		ch.prefetch(1);
 		const consumeCallback = msg => {
 			console.log(`got message: ${msg.content.toString()}`);
@@ -48,6 +50,8 @@ amqp.connect(AMQP_ADDRESS, (err, conn) => {
 		ch.consume(READ_QUEUE, consumeCallback);
 		console.log(`consuming on ${WRITE_QUEUE}`);
 		ch.consume(WRITE_QUEUE, consumeCallback);
+		console.log(`consuming on ${TASK_QUEUE}`);
+		ch.consume(TASK_QUEUE, consumeCallback);
 	});
 });
 
