ef8e02682cf2cbac7681e0c55687d65df270aa5e
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-07-31 10:29:11 -0400

    updated schema

--- quizzes/quizzes/miniexample/db_migrations/20180724101120_create_miniexample_users.js
+++ quizzes/quizzes/miniexample/db_migrations/20180724101120_create_miniexample_users.js
@@ -2,8 +2,10 @@ exports.up = function(knex) {
   return knex.schema.createTable('miniexample_users', table => {
     table.increments('id').primary();
     table.string('auth0_id');
-    table.timestamp('created_at');
+    table.timestamp('created_at').notNullable();
     table.timestamp('updated_at');
+		table.date('dob');
+		table.string('native_language');
   });
 };
 
