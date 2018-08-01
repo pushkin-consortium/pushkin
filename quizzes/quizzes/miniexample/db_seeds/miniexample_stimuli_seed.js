c185b27d1818e34ccb3df8de36a22e821b61f26d
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-08-01 15:17:34 -0400

    added seeds

--- quizzes/quizzes/miniexample/db_seeds/miniexample_stimuli_seed.js
+++ quizzes/quizzes/miniexample/db_seeds/miniexample_stimuli_seed.js
@@ -1,12 +1,33 @@
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('miniexample_stimuli').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('miniexample_stimuli').insert([
-				{ task: 'task1', stimulus: `What's your favorite color?`, options: `orange, green, blue`, num_responses: 0 },
-				{ task: 'task2', stimulus: `What's your favorite number?`, options: `7, 8, 9`, num_responses: 0 }
-      ]);
-    });
+	// Deletes ALL existing entries
+	return knex('miniexample_stimuli').del()
+		.then(function () {
+			// Inserts seed entries
+
+			const colors = [ 'red', 'yellow', 'blue', 'robin egg blue', 'white', 'swamp green', 'fern green', 'grass green' ];
+
+			const tests = [
+				{ p: 'What color is the sky?', a: 'blue' },
+				{ p: 'What color is grass?', a: 'grass green' },
+				{ p: 'What color is blue?', a: 'blue' }
+			];
+			const trickyQuestions = [
+				{ p: 'What color are ovals?', a: 'white' },
+				{ p: 'What color is air?', a: 'blue' },
+				{ p: 'Are you sure?', a: 'red' },
+				{ p: 'What color are robins?', a: 'robin egg blue' },
+				{ p: 'Hmm... guess again.', a: 'robin egg blue' },
+			];
+
+			const insertData = [ ...tests, ...trickyQuestions ].map(q => ({
+				stimulus: {
+					type: 'html-button-response',
+					stimulus: q.p,
+					choices: colors
+				},
+				correct: q.a
+			}));
+			return knex('miniexample_stimuli').insert(insertData);
+		});
 };
