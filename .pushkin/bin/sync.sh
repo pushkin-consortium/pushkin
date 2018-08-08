9734754d4e103d5734006a087a09d98ee85a8b72
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-08-08 13:03:31 -0400

    added specific sync commands

--- .pushkin/bin/sync.sh
+++ .pushkin/bin/sync.sh
@@ -10,7 +10,7 @@ pushkin_conf_dir="$PWD"/.pushkin
 
 source "${pushkin_conf_dir}/pushkin_config_vars.sh"
 source "${pushkin_conf_dir}/bin/core.sh"
-#source "${pushkin_env_file}"
+source "${pushkin_env_file}"
 set +e
 
 ##############################################
@@ -31,8 +31,21 @@ set +e
 toSync="${1}"
 shift
 case "${toSync}" in
-	"core")
-		.pushkin/bin/syncCoreDockers.sh "$@"
+	"api")
+		log "syncing docker api as '${pushkin_api_docker_name}'"
+		docker push "${image_prefix}/${pushkin_api_docker_name}${image_tag}"
+		;;
+	"cron")
+		log "syncing docker cron as '${pushkin_cron_docker_name}'"
+		docker push "${image_prefix}/${pushkin_cron_docker_name}${image_tag}"
+		;;
+	"dbworker")
+		log "syncing docker db_worker as '${pushkin_db_worker_docker_name}'"
+		docker push "${image_prefix}/${pushkin_db_worker_docker_name}${image_tag}"
+		;;
+	"server")
+		log "syncing docker server as '${pushkin_server_docker_name}'"
+		docker push "${image_prefix}/${pushkin_server_docker_name}${image_tag}"
 		;;
 	"quizzes")
 		.pushkin/bin/syncQuizzes.sh "$@"
@@ -40,13 +53,19 @@ case "${toSync}" in
 	"website")
 		.pushkin/bin/syncWebsite.sh "$@"
 		;;
+	"core")
+		.pushkin/bin/sync.sh api
+		.pushkin/bin/sync.sh cron
+		.pushkin/bin/sync.sh dbworker
+		.pushkin/bin/sync.sh server
+		;;
 	"all")
 		.pushkin/bin/sync.sh core
 		.pushkin/bin/sync.sh quizzes
 		.pushkin/bin/sync.sh website
 		;;
 	*)
-		die "sync can sync 'core', 'quizzes', 'website', or 'all'"
+		die "sync can sync 'api', 'server', 'dbworker', 'cron', 'core', 'quizzes', 'website', or 'all'"
 		;;
 esac
 
