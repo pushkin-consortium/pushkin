9d6fed5d315c7eb0a7e0c2c9bbd83ffa3e19c2f4
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-08-14 14:43:43 -0400

    templatized sessions

--- .pushkin/newQuizTemplates/worker/index.js
+++ .pushkin/newQuizTemplates/worker/index.js
@@ -2,6 +2,7 @@ const AMQP_ADDRESS = process.env.AMQP_ADDRESS || 'amqp://localhost';
 const READ_QUEUE = '${QUIZ_NAME}_quiz_dbread';
 const WRITE_QUEUE = '${QUIZ_NAME}_quiz_dbwrite';
 const TASK_QUEUE = '${QUIZ_NAME}_quiz_taskworker'; // eventually this should be moved to a separate thing
+const trim = require('./trim').trim;
 
 const amqp = require('amqplib/callback_api');
 const Handler = new (require('./handler'))();
@@ -21,14 +22,14 @@ amqp.connect(AMQP_ADDRESS, (err, conn) => {
 		ch.assertQueue(TASK_QUEUE, {durable: false});
 		ch.prefetch(1);
 		const consumeCallback = msg => {
-			console.log(`got message: ${msg.content.toString()}`);
+			console.log(`got message: ${trim(msg.content.toString(), 100)}`);
 			Promise.resolve(msg.content.toString())
 				.then(JSON.parse)
 				// nobody should ever have to do this
 				.then(Handler.handle.bind(Handler))
 				.then(res => {
 					if (res || res == 0) { // 0 is used for success by "void" methods
-						console.log(`responding ${JSON.stringify(res)} ${res==0?'(success)':''}`);
+						console.log(`responding ${trim(JSON.stringify(res), 100)} ${res==0?'(success)':''}`);
 						ch.sendToQueue(msg.properties.replyTo,
 							new Buffer.from(JSON.stringify(res)),
 							{correlationId: msg.properties.correlationId}
