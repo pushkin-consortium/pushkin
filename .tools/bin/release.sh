ce4f687224442a2930a72665ac3d36856074a3e4
jkhartshorne <>
2019-01-20 14:39:21 -0500

    Fixing conflicts between different versions of the CLT. Creating dev tools with a 'release' tool.

---
+++ .tools/bin/release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "version:"
+read version
+
+echo "description:"
+read description
+
+zip -r pushkin.zip * .pushkin -x *.git*
+
+git tag $version && git push --tags
+
+gothub release \
+    --user pushkin-consortium \
+    --repo pushkin \
+    --tag $version \
+    --name $version \
+    -d $description
+
+gothub upload \
+    --user pushkin-consortium \
+    --repo pushkin \
+    --tag $version \
+    --name "pushkin" \
+    --file pushkin.zip \
+    --replace
+
+rm pushkin.zip
