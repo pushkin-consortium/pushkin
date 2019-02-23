344f4d94f8a9a88fa47e306e0fe9b8f1860d893d
sc8ing <16541352+sc8ing@users.noreply.github.com>
2019-02-23 13:32:51 -0500

    added eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "warn",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "warn",
+            "single"
+        ],
+        "semi": [
+            "warn",
+            "always"
+				],
+			"no-console": "off",
+			"no-unused-vars": "warn"
+    }
+};
