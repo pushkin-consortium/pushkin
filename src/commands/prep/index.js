b81d30b4efb91e051643566a2b8c5548f8c56005
sc8ing <16541352+sc8ing@users.noreply.github.com>
2019-03-16 19:35:39 -0400

    started thinking

--- src/commands/prep/index.js
+++ src/commands/prep/index.js
@@ -1,5 +1,13 @@
 import path from 'path';
 
 export default (experimentsDir, coreDir) => {
-	console.log('running prep command');
+	// 1. run this for each experiment found in experimentsDir
+	// 2. read in the experiment's config file
+	// API CONTROLLER:
+	// template needs to read in from a folder of controllers with config (attachment) information
+	// require each one and attach to the appropriate spot
+	// prep needs to copy?
+	//
+	// WORKER:
+	// WEB PAGE:
 };
