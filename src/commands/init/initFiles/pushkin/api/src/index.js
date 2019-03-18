fcf59a83ed68db0f1629f3c207e78c947511b9fa
sc8ing <16541352+sc8ing@users.noreply.github.com>
2019-03-18 18:46:05 -0400

    updated included template files from testing (works with api prep)

--- src/commands/init/initFiles/pushkin/api/src/index.js
+++ src/commands/init/initFiles/pushkin/api/src/index.js
@@ -1,5 +1,6 @@
 import pushkin from 'pushkin-api';
-import templateController from './controllers/template/';
+import fs from 'fs';
+import path from 'path';
 
 const port = process.env.PORT || 3000;
 const amqpAddress = process.env.AMQP_ADDRESS || 'amqp://localhost:5672';
@@ -8,7 +9,15 @@ const api = new pushkin.API(port, amqpAddress);
 
 api.init()
 	.then(() => {
-		api.usePushkinController('/api/template', templateController);
+		// load in user controllers
+		const controllersFile = path.join(__dirname, 'controllers.json');
+		const controllers = JSON.parse(fs.readFileSync(controllersFile));
+		controllers.forEach(controller => {
+			const mountPath = path.join('/api/', controller.mountPath);
+			const contrModule = require(controller.name);
+			console.log(contrModule);
+			api.usePushkinController(mountPath, contrModule);
+		});
 		api.start();
 	})
 	.catch(console.error);
