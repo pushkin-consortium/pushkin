e26ccfa6103e12f234ceb63e9bbbc75b8cb127be
sc8ing <16541352+sc8ing@users.noreply.github.com>
2019-03-25 15:15:07 -0400

    db conn args separated into fields

--- src/commands/generate/generateFiles/template/worker/index.js
+++ src/commands/generate/generateFiles/template/worker/index.js
@@ -6,7 +6,13 @@ const options = {
 	taskQueue: 'template_quiz_taskworker',
 	amqpAddress: process.env.AMQP_ADDRESS || 'amqp://localhost'
 };
-const db_url = process.env.DATABASE_URL || 'postgres://postgres:@localhost/test_db';
+
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_url = process.env.DB_URL;
+const db_name = process.env.DB_NAME;
+
+const db_conn_address = `postgres://${db_user}:${db_pass}@${db_url}/${db_name}`;
 
 const worker = new pWorker(options);
 worker.init()
@@ -15,7 +21,7 @@ worker.init()
 			console.log(`handling test method got data: ${data}`);
 			return `successfully got ${data}`;
 		});
-		worker.useDefaultHandles(db_url, 'template');
+		worker.useDefaultHandles(db_conn_address, 'template');
 		worker.start();
 	})
 	.catch(err => {
