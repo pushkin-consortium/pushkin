3c6787545fe6721333bdbefb07d6b301ebfc404c
sc8ing <16541352+sc8ing@users.noreply.github.com>
2019-04-08 16:08:17 -0400

    custom httpmethods for uses allowed

--- src/controllerBuilder.js
+++ src/controllerBuilder.js
@@ -6,11 +6,16 @@ export default class ControllerBuilder {
 	constructor() {
 		this.passAlongs = [];
 		this.directUses = [];
+		this.validHttpMethods = ['get', 'post', 'put', 'delete'];
 	}
 
-	// pass posts on this route to this method via this queue
-	setPass(route, method, queue) {
-		this.passAlongs.push({ route, method, queue });
+	// pass posts/gets/puts/deletes on this route to this method via this queue
+	setPass(route, rpcMethod, queue, httpMethod) {
+		if (this.validHttpMethods.indexOf(httpMethod) < 0) {
+			console.error(`'${httpMethod}' is not a valid http method. Ignoring for route ${route}`);
+			return;
+		}
+		this.passAlongs.push({ route, rpcMethod, queue, httpMethod });
 	}
 
 	// allow users to set their own custom api endpoints that don't just pass things along
@@ -20,11 +25,11 @@ export default class ControllerBuilder {
 	}
 
 	setDefaultPasses(readQueue, writeQueue, taskQueue) {
-		this.setPass('/startExperiment', 'startExperiment', taskQueue);
-		this.setPass('/getStimuli', 'getStimuli', readQueue);
-		this.setPass('/metaResponse', 'insertMetaResponse', writeQueue);
-		this.setPass('/stimulusResponse', 'insertStimulusResponse', writeQueue);
-		this.setPass('/endExperiment', 'endExperiment', taskQueue);
+		this.setPass('/startExperiment', 'startExperiment', taskQueue, 'post');
+		this.setPass('/getStimuli', 'getStimuli', readQueue, 'post');
+		this.setPass('/metaResponse', 'insertMetaResponse', writeQueue, 'post');
+		this.setPass('/stimulusResponse', 'insertStimulusResponse', writeQueue, 'post');
+		this.setPass('/endExperiment', 'endExperiment', taskQueue, 'post');
 	}
 
 	getConnFunction() {
@@ -32,26 +37,26 @@ export default class ControllerBuilder {
 			const router = new express.Router();
 
 			this.passAlongs.forEach(point => 
-				router.post(point.route, (req, res, next) => { // eslint-disable-line
+				route[point.httpMethod](point.route, (req, res, next) => { // eslint-disable-line
 
-					console.log(`${point.route} hit (in router conn function)`);
+					console.log(`${point.httpMethod.toUpperCase()} ${point.route} hit (in router conn function)`);
 					console.log(req.body);
 
 					const rpcParams = {
-						method: point.method,
+						method: point.rpcMethod,
 						data: req.body,
+						params: req.params,
 						sessionId: req.session.id
 					};
 
 					rpc(conn, point.queue, rpcParams)
 						.then(rpcRes => {
-							try { console.log(`${point.method} method rpc response: ${trim(JSON.stringify(rpcRes), 100)}`); }
-							catch (e) { console.log(`${point.method} method rpc response (failed to JSON.stringify): ${trim(rpcRes, 100)}`); }
+							try { console.log(`${point.rpcMethod} method rpc response: ${trim(JSON.stringify(rpcRes), 100)}`); }
+							catch (e) { console.log(`${point.rpcMethod} method rpc response (failed to JSON.stringify): ${trim(rpcRes, 100)}`); }
 							res.send({ resData: rpcRes });
 						})
 						.catch(rpcErr => {
-							console.log('Error in API getting RPC response:');
-							console.log(rpcErr);
+							console.error(`Error in API getting RPC response: ${rpcErr}`);
 							res.status(500).send(rpcErr);
 						});
 				})
