380292e0ee399099b8767aee71a038df1721a834
sc8ing <16541352+sc8ing@users.noreply.github.com>
2019-04-16 17:25:42 -0400

    logging cleaned up, added back db name accidental deletion

--- src/index.js
+++ src/index.js
@@ -73,7 +73,7 @@ const nextArg = inputGetter();
 		case 'hardreset': {
 			exec('docker-compose down; docker rm -f $(docker ps -a -q); docker volume rm $(docker volume ls -q);', (err, stdout, stderr) => {
 			  if (err) {
-    			console.log("node couldn't execute the command")
+    			console.log(`Error resetting docker containers and volumes: ${err}`);
 			    return;
 			  }
 			})
