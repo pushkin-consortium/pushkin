6b03b0b37b32bfb085e9c08a03185073630ada23
“jkhartshorne” <jkhartshorne@gmail.com>
2019-04-25 15:38:53 -0400

    Removed unused actions from front-end, along with reducers.

--- src/commands/init/initFiles/pushkin/front-end/src/site/reducers/index.js
+++ src/commands/init/initFiles/pushkin/front-end/src/site/reducers/index.js
@@ -1,15 +1,11 @@
 import { routerReducer } from 'react-router-redux';
 import { combineReducers } from 'redux';
-import error from './error';
 import userInfo from './userinfo';
 import forum from './forum';
 import tempResponses from './tempResponse';
-import nextpage from './nextpage';
 import { reducer as formReducer } from 'redux-form';
 
 export const rootReducer = combineReducers({
-  error,
-  nextpage,
   userInfo,
   routing: routerReducer,
   form: formReducer,
