4cc022b865fb781cb9c149112c968c486a798d22
jkhartshorne <6118115+jkhartshorne@users.noreply.github.com>
2019-12-12 20:28:04 -0500

    Fixed a bug in useHandler()

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import amqp from 'amqplib';
-import { defaultHandler, defaultMethods }from './defaultHandler.js';
+import knex from 'knex';
+const trim = (s, len) => s.length > len ? s.substring(0, len) : s;
 
-export default class Worker {
+class PushkinWorker {
 	constructor(options) {
 		this.amqpAddress = options.amqpAddress;
 		this.readQueue = options.readQueue;
@@ -31,9 +32,16 @@ export default class Worker {
 		console.log(`handling ${method} with ${handler}`);
 		this.handlers.set(method, handler);
 	}
-	useDefaultHandles(dbUrl, dbTablePrefix, transactionOps) {
-		const handler = new defaultHandler(dbUrl, dbTablePrefix, transactionOps);
-		defaultMethods.forEach(h => {
+	// useDefaultHandles(dbUrl, dbTablePrefix, transactionOps) {
+	// 	const handler = new defaultHandler(dbUrl, dbTablePrefix, transactionOps);
+	// 	defaultMethods.forEach(h => {
+	// 		this.handle(h, handler[h].bind(handler));
+	// 	});
+	// }
+	useHandler(ahandler, dbUrl, dbTablePrefix, transactionOps) {
+		const handler = new ahandler(dbUrl, dbTablePrefix, transactionOps);
+		let methods = handler.methods();
+		methods.forEach(h => {
 			this.handle(h, handler[h].bind(handler));
 		});
 	}
@@ -88,3 +96,112 @@ export default class Worker {
 			});
 	}
 }
+
+// helper function for turning string *or* JSON into string
+function handleJSON(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return str;
+    }
+    return JSON.stringify(str);
+}
+
+class DefaultHandler {
+	constructor(db_url, dbTablePrefix, transactionOps) {
+		this.tables = {
+			users: `${dbTablePrefix}_users`,
+			stim: `${dbTablePrefix}_stimuli`,
+			stimResp: `${dbTablePrefix}_stimulusResponses`,
+			stimGroups: `${dbTablePrefix}_stimulusGroups`,
+			stimGroupStim: `${dbTablePrefix}_stimulusGroupStimuli`
+		};
+		this.pg_main = knex({ client: 'pg', connection: db_url, });
+
+		this.logging = transactionOps ? true : false;
+		if (this.logging) {
+			this.trans_table = transactionOps.tableName;
+			this.pg_trans = knex({ client: 'pg', connection: transactionOps.url });
+			this.trans_mapper = transactionOps.mapper;
+		}
+	}
+
+	async logTransaction(knexCommand) {
+		if (!this.logging) return knexCommand;
+		const toInsert = this.trans_mapper(knexCommand.toString());
+		await this.pg_trans(this.trans_table).insert(toInsert);
+		return knexCommand;
+	}
+
+	async startExperiment(sessId, data) {
+		console.log(arguments);
+		if (!sessId)
+			throw new Error('startExperiment got invalid session id');
+		if (!data.user_id)
+			throw new Error('startExperiment got invalid userID');
+
+		const userId = data.user_id;
+
+		await this.logTransaction(this.pg_main(this.tables.users).insert({
+			user_id: data.user_id,
+			created_at: new Date()
+		})); 
+		//using 'await' because stimulus recording will fail if user isn't initialized yet.
+		//not likely to happen, but stranger things have happened...
+
+		return true;
+	}
+
+	async getStimuli(sessId, data) {
+		// nItems = 0 implies get all stimuli
+		if (!sessId)
+			throw new Error('getStimuli got invalid session id');
+		if (!data.user_id)
+			throw new Error('getStimuli got invalid userID');
+
+		const userId = data.user_id;
+		const nItems = data.nItems;
+		const selectedStims = (data.nItems ? 
+			await this.pg_main(this.tables.stim).select('stimulus').orderByRaw('random()').limit(nItems) :
+			await this.pg_main(this.tables.stim).select('stimulus').orderByRaw('random()')
+			);
+		console.log(selectedStims);
+
+		return selectedStims;
+	}
+
+	async insertStimulusResponse(sessId, data) {
+		if (!sessId)
+			throw new Error('insertStimulusResponse got invalid session id');
+		if (!data.data_string)
+			throw new Error('insertStimulusResponse got invalid response data string');
+		if (!data.user_id)
+			throw new Error('insertStimulusResponse got invalid userID');
+		if (!data.data_string.stimulus)
+			throw new Error('insertStimulusResponse got invalid stimulus');
+
+		console.log(`inserting response for user ${data.user_id}: ${trim(JSON.stringify(data.data_string), 100)}`);
+
+		return this.logTransaction(this.pg_main(this.tables.stimResp).insert({
+			user_id: data.user_id,
+			stimulus: handleJSON(data.stimulus),
+			response: JSON.stringify(data.data_string),
+			created_at: new Date()
+		}));
+	}
+
+	methods() {
+		const methods = [
+			'getStimuli',
+			'insertStimulusResponse',
+			'startExperiment'
+		]
+		return methods;
+	}
+}
+
+module.exports = {
+	defaultHandler: DefaultHandler,
+	pushkinWorker: PushkinWorker
+};
+
