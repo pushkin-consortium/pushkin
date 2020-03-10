004df466230e76afb35d11ded6c55db7d8585475
hrcn <hrcn1128@gmail.com>
2020-03-10 16:43:12 -0400

    load script urls

--- tests/client.test.js
+++ tests/client.test.js
@@ -5,17 +5,25 @@ import Pushkin from '../src/index';
 // we can use the jest.mock(...) function to automatically mock the axios module.
 jest.mock('axios');
 
-const pushkinClient = new Pushkin();
-
 const quizURL = './api/quiz';
 axios.create.mockResolvedValue(quizURL);
 // or you could use the following depending on your use case:
 // axios.get.mockImplementation(() => Promise.resolve(quizURL))
 
+const pushkinClient = new Pushkin();
 pushkinClient.connect(quizURL);
 
-test('should connect to quiz api url', () => {
+test('connect to quiz api url', () => {
   // Pushkin { con: Promise { './api/quiz' } }
-  return pushkinClient.con.then((data) => expect(data).toEqual(quizURL));
+  pushkinClient.con.then((data) => expect(data).toEqual(quizURL));
 });
 
+test('load script url', () => {
+  const testURL = 'test';
+  pushkinClient.loadScript(testURL).then((data) => expect(data).toEqual(testURL));
+})
+
+test('load multiple script urls', () => {
+  const testURLs = ['test1', 'test2', 'test3'];
+  pushkinClient.loadScripts(testURLs).then((data) => expect(data).toEqual(testURLs));
+})
