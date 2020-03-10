81114b6cba9c8ca091ce5176ca26519cb3185985
hrcn <hrcn1128@gmail.com>
2020-03-10 12:32:08 -0400

    should connect to quiz api url

--- tests/client.test.js
+++ tests/client.test.js
@@ -1,15 +1,21 @@
 import axios from 'axios';
 import Pushkin from '../src/index';
 
+// in order to test this method without actually hitting the API (creating slow and fragile tests),
+// we can use the jest.mock(...) function to automatically mock the axios module.
 jest.mock('axios');
 
 const pushkinClient = new Pushkin();
 
 const quizURL = './api/quiz';
 axios.create.mockResolvedValue(quizURL);
-pushkinClient.connect('./api/quiz');
+// or you could use the following depending on your use case:
+// axios.get.mockImplementation(() => Promise.resolve(quizURL))
 
-test('check if the constructor was called', () => {
-  console.log(pushkinClient);
+pushkinClient.connect(quizURL);
+
+test('should connect to quiz api url', () => {
+  // Pushkin { con: Promise { './api/quiz' } }
   return pushkinClient.con.then((data) => expect(data).toEqual(quizURL));
 });
+
