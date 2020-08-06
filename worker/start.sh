89da35ce5cdb4ef28e65c0f15b5fd7615af9dd26
Connie <con.bainbridge@gmail.com>
2020-08-06 12:17:59 -0400

    Fix typo in start.sh

--- worker/start.sh
+++ worker/start.sh
@@ -2,5 +2,6 @@
 echo "pushkintemplate worker loaded, waiting for rabbitmq"
 AMPQ_DOMAIN=$(echo $AMPQ_ADDRESS | sed -e "s/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/")
 echo "Waiting for port 5672 on $AMPQ_DOMAIN"
-while ! nc -z $AMPQ_DOMAIN 5672; do sleep 3; doneecho "Rabbitmq loaded"
+while ! nc -z $AMPQ_DOMAIN 5672; do sleep 3; done
+echo "Rabbitmq loaded"
 node index.js
