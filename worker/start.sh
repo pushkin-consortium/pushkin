8e5c532568902072266db25201f77d3d7df59a2b
jkhartshorne <6118115+jkhartshorne@users.noreply.github.com>
2020-08-13 10:11:55 -0400

    Fixed typos in AMQP variables.

--- worker/start.sh
+++ worker/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Worker started. Waiting for rabbitMQ"
-AMPQ_DOMAIN=$(echo $AMPQ_ADDRESS | sed -e "s/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/")
-echo "Waiting for port 5672 on $AMPQ_DOMAIN"
-while ! nc -z $AMPQ_DOMAIN 5672; do sleep 3; done
+AMQP_DOMAIN=$(echo $AMQP_ADDRESS | sed -e "s/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/")
+echo "Waiting for port 5672 on $AMQP_DOMAIN"
+while ! nc -z $AMQP_DOMAIN 5672; do sleep 3; done
 echo "Rabbitmq loaded"
 node index.js
