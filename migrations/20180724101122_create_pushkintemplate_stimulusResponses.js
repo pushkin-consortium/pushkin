4bb4f36c729f74adc2bdcaebd1f85229632e6bec
jkhartshorne <6118115+jkhartshorne@users.noreply.github.com>
2020-08-13 10:30:19 -0400

    Fixed some typos for deployment

--- migrations/20180724101122_create_pushkintemplate_stimulusResponses.js
+++ migrations/20180724101122_create_pushkintemplate_stimulusResponses.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('pushkintemplate_stimulusResponses', table => {
     table.increments('id').primary();
     table.string('user_id').references('user_id').inTable('pushkin_users').notNullable();
-    table.string('stimulus').notNullable();
+    table.string('stimulus', 1000).notNullable();
     table.json('response').notNullable();
     table.timestamp('created_at').notNullable();
     table.timestamp('updated_at');
