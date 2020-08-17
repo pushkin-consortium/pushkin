f3cb4324145bbe23b1daf87fcbbecba52ecf0248
jkhartshorne <6118115+jkhartshorne@users.noreply.github.com>
2020-08-17 16:59:50 -0400

    Completed front-end deploy, sans authentication. Still need to make sure local testing was not broken.

--- src/commands/aws/index.js
+++ src/commands/aws/index.js
@@ -147,7 +147,7 @@ const deployFrontEnd = async (projName, awsName, useIAM, myDomain, myCertificate
   policy.Statement[0].Resource = "arn:aws:s3:::".concat(awsName).concat("/*")
   try {
     await exec(`aws s3 website s3://${awsName} --profile ${useIAM} --index-document index.html --error-document index.html`)
-    await (`aws s3api put-bucket-policy --bucket `.concat(awsName).concat(` --policy '`).concat(JSON.stringify(policy)).concat(`' --profile ${useIAM}`))
+    await exec(`aws s3api put-bucket-policy --bucket `.concat(awsName).concat(` --policy '`).concat(JSON.stringify(policy)).concat(`' --profile ${useIAM}`))
   } catch (e) {
     console.error('Problem setting bucket permissions for front-end')
     throw e
