4bda18066cf88fe0cf1f9da81516e83f62782383
Joshua Hartshorne <languagelearninglab@bc.edu>
2023-07-21 12:28:42 -0400

    yet more yarn stabilization.

--- src/commands/aws/index.js
+++ src/commands/aws/index.js
@@ -89,11 +89,11 @@ const buildFE = function (projName) {
     let buildCmd
     if (packageJson.dependencies['build-if-changed'] == null) {
       console.log(modName, " does not have build-if-changed installed. Recommend installation for faster runs of prep.")
-      buildCmd = pacMan.concat(' run build')
+      buildCmd = pacMan.concat(' run build --mutex network')
     } else {
       console.log("Using build-if-changed for ",projName)
       const pacRunner = (pacMan == 'yarn') ? 'yarn' : 'npx'
-      buildCmd = pacRunner.concat(' build-if-changed')
+      buildCmd = pacRunner.concat(' build-if-changed --mutex network')
     }
     let builtWeb
     console.log("Building combined front-end")
