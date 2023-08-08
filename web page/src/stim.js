1c484a0beabf58b00dbe5d902b411b4f71c288db
Jesse Storbeck <jesse.storbeck@bc.edu>
2023-08-08 09:20:10 -0400

    overwrite stim and config files for grammaticality

--- web page/src/stim.js
+++ web page/src/stim.js
@@ -1,25 +1,10 @@
 // Example stimuli
 
 const stimArray = [
-    { word_1: 'SOCKS', word_2: 'SHOE', both_words: true, related: true },
-    { word_1: 'SLOW', word_2: 'FAST', both_words: true, related: true },
-    { word_1: 'QUEEN', word_2: 'KING', both_words: true, related: true },
-    { word_1: 'LEAF', word_2: 'TREE', both_words: true, related: true },
-
-    { word_1: 'SOCKS', word_2: 'TREE', both_words: true, related: false },
-    { word_1: 'SLOW', word_2: 'SHOE', both_words: true, related: false },
-    { word_1: 'QUEEN', word_2: 'FAST', both_words: true, related: false },
-    { word_1: 'LEAF', word_2: 'KING', both_words: true, related: false },
-
-    { word_1: 'AGAIN', word_2: 'PLAW', both_words: false, related: false },
-    { word_1: 'BOARD', word_2: 'TRUDE', both_words: false, related: false },
-    { word_1: 'LIBE', word_2: 'HAIR', both_words: false, related: false },
-    { word_1: 'MOCKET', word_2: 'MEET', both_words: false, related: false },
-
-    { word_1: 'FLAFF', word_2: 'PLAW', both_words: false, related: false },
-    { word_1: 'BALT', word_2: 'TRUDE', both_words: false, related: false },
-    { word_1: 'LIBE', word_2: 'NUNE', both_words: false, related: false },
-    { word_1: 'MOCKET', word_2: 'FULLOW', both_words: false, related: false }
+    { sentence_grammatical: 'This is a sentence', sentence_ungrammatical: 'This a sentence' },
+    { sentence_grammatical: 'He went to the park last night.', sentence_ungrammatical: 'He went the park last night' },
+    { sentence_grammatical: 'The cow is jumping.', sentence_ungrammatical: 'The cow are jumping.' },
+    { sentence_grammatical: 'Which friend did she meet with?', sentence_ungrammatical: 'Which friend she meet with did?' }
 ]
 
 export default stimArray;
