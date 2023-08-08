93158139d3b8414f01ec68afa7168b06005d8488
Jesse Storbeck <jesse.storbeck@bc.edu>
2023-08-08 09:07:44 -0400

    remove everything

--- web page/src/debrief.js
+++
@@ -1,5 +0,0 @@
-const debrief = '<p>' +
-    'Debrief text goes here. In this study, we are interested in understanding performance on identifying grammatical and ungrammatical sentences.' +
-    '</p>'
-
-export default debrief;
