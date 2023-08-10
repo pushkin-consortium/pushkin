4be387554fb4015c1b5548d91c51411dfffea4b0
Jesse Storbeck <jesse.storbeck@bc.edu>
2023-08-10 16:47:28 -0400

    delete everything

--- README.md
+++
@@ -1,4 +0,0 @@
-<img src="/web page/src/assets/pushkin_bw_w_text.png" height="400" width="450">
-
-# pushkin-exptemplates-reading
-Basic jsPsych self-paced reading experiment
--- web page/src/debrief.js
+++
@@ -1,5 +0,0 @@
-const debrief = '<p>' +
-    'Debrief text goes here. In this study, we are interested in understanding how people\'s rate of reading varies based on the context and structure of a sentence, and whether or not ambiguous words with multiple meanings are used.' +
-    '</p>'
-
-export default debrief;
