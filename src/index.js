8910eab7d5a9a0343fbefc69e68ae73bdc2ac8bb
Joshua Hartshorne <languagelearninglab@bc.edu>
2023-08-21 14:10:24 -0400

    Removed core-js. Doesn't seem to be used for anything.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@
 import "regenerator-runtime/runtime";
 import API from './api.js';
 import ControllerBuilder from './controllerBuilder.js';
-//import "core-js/stable";
 
 module.exports = { API, ControllerBuilder };
