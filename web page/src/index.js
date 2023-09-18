bdbd5f49892ed9b2bc274ad1617e12b4b7612d71
Jesse Storbeck <jesse.storbeck@bc.edu>
2023-09-18 14:05:21 -0400

    add config var

--- web page/src/index.js
+++ web page/src/index.js
@@ -7,7 +7,8 @@ import jsYaml from 'js-yaml';
 const fs = require('fs');
 
 //stylin'
-import './assets/experiment.css'
+import './assets/experiment.css';
+const experimentConfig = require('./config').default;
 
 const expConfig = jsYaml.load(fs.readFileSync('../config.yaml'), 'utf8');
 
