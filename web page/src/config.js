c5fc67c18c82207759a5d90686b11e64b66f935e
Jesse Storbeck <jesse.storbeck@bc.edu>
2023-09-18 15:25:26 -0400

    add back config files etc

---
+++ web page/src/config.js
@@ -0,0 +1,10 @@
+// Custom stylin'
+
+var experimentConfig = {
+    fontColor: "black",
+    fontSize: "20px",
+    fontFamily: "'Open Sans', 'Arial', sans-serif",
+    correctiveFeedback: true
+}
+
+export default experimentConfig;
