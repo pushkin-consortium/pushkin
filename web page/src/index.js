d52cf8b13c9b840b3fb007e5348014e754d59ce2
Jesse Storbeck <jesse.storbeck@bc.edu>
2023-09-18 15:32:14 -0400

    add config settings

--- web page/src/index.js
+++ web page/src/index.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 
 //stylin'
 import './assets/experiment.css'
+import experimentConfig from './config';
 
 const expConfig = jsYaml.load(fs.readFileSync('../config.yaml'), 'utf8');
 
@@ -49,6 +50,12 @@ class quizComponent extends React.Component {
     jsPsych.run(timeline);
 
     document.getElementById('jsPsychTarget').focus();
+    
+    // Settings from config file
+    document.getElementById('jsPsychTarget').style.color = experimentConfig.fontColor;
+    document.getElementById('jsPsychTarget').style.fontSize = experimentConfig.fontSize;
+    document.getElementById('jsPsychTarget').style.fontFamily = experimentConfig.fontFamily;
+    
     this.setState({ loading: false });
   }
 
