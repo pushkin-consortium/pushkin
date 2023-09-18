a6b4f364a223ae6bc091e3a564cf371efcf93d16
Jesse Storbeck <jesse.storbeck@bc.edu>
2023-09-18 10:54:09 -0400

    add back config files etc

---
+++ web page/src/config.js
@@ -0,0 +1,12 @@
+// Set configurations for experiment
+// Options for responseType are "2afc" for a 2-alternative forced choice, "likert" for a likert scale, or "slider" for a 0-100 sliding scale
+
+const experimentConfig = {
+    fontColor: "black",
+    fontSize: "20px",
+    fontFamily: "'Open Sans', 'Arial', sans-serif",
+    correctiveFeedback: true,
+    responseType: "2afc"
+}
+
+export default experimentConfig;
