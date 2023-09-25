5a6c76a5c111e4577c19e2aea1bdedac9c8776b1
Jesse Storbeck <jesse.storbeck@bc.edu>
2023-09-25 11:48:49 -0400

    add config info to index.js

--- web page/src/index.js
+++ web page/src/index.js
@@ -7,7 +7,8 @@ import jsYaml from 'js-yaml';
 const fs = require('fs');
 
 //stylin'
-import './assets/experiment.css'
+import './assets/experiment.css';
+import experimentConfig from './config';
 
 const expConfig = jsYaml.load(fs.readFileSync('../config.yaml'), 'utf8');
 
@@ -49,6 +50,12 @@ class quizComponent extends React.Component {
     jsPsych.run(timeline);
 
     document.getElementById('jsPsychTarget').focus();
+    
+    // Settings from config file
+    document.getElementById('jsPsychTarget').style.color = experimentConfig.fontColor;
+    document.getElementById('jsPsychTarget').style.fontSize = experimentConfig.fontSize;
+    document.getElementById('jsPsychTarget').style.fontFamily = experimentConfig.fontFamily;
+
     this.setState({ loading: false });
   }
 
