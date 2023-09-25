7518f778e6675ab2b1db4b79d7d25940357141a4
Jesse Storbeck <jesse.storbeck@bc.edu>
2023-09-25 11:38:36 -0400

    add back config files etc

---
+++ web page/src/config.js
@@ -0,0 +1,11 @@
+// Custom stylin'
+
+const experimentConfig = {
+    fontColor: "black",
+    fontSize: "20px",
+    fontFamily: "'Open Sans', 'Arial', sans-serif",
+    comprehension: true,
+    correctiveFeedback: true // Only relevant if comprehension is set to true
+}
+
+export default experimentConfig;
---
+++ web page/src/debrief.js
@@ -0,0 +1,5 @@
+const debrief = `
+    <p>Debrief text goes here. In this study, we are interested in understanding how people's rate of reading varies based on the context and structure of a sentence, and whether or not ambiguous words with multiple meanings are used.</p>
+`
+
+export default debrief;
