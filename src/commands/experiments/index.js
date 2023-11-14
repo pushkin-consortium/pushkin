8aeff5cd3e5734e58867f934f42af00c9d1b5c28
Jesse Storbeck <jesse.storbeck@bc.edu>
2023-11-14 11:12:12 -0500

    giving up on yaml name issue

--- src/commands/experiments/index.js
+++ src/commands/experiments/index.js
@@ -321,8 +321,10 @@ const initExperiment = async (expDir, expName, longName, rootDir, verbose) => {
     throw err;
   }
   
+  // It seems like we should be able to switch names in the experiment template yaml to 'pushkintemplate'
+  // and avoid the need for these lines, but it doesn't work
   expConfig.experimentName = longName;
-  //expConfig.shortName = expName; // Should be unnecessary
+  expConfig.shortName = expName;
 
   try {
     fs.writeFileSync(path.join(expDir, 'config.yaml'), jsYaml.safeDump(expConfig), 'utf8');
