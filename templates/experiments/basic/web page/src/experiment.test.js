6573a28f1568fd107395eb1d692f6a248fe702b6
Jesse Storbeck <jesse.storbeck@bc.edu>
2023-11-27 16:29:20 -0500

    pass jsPsych argument to createTimeline in tests

--- templates/experiments/basic/web page/src/experiment.test.js
+++ templates/experiments/basic/web page/src/experiment.test.js
@@ -2,15 +2,22 @@
  * @jest-environment jsdom
  */
 
-import { createTimeline } from "./experiment";
+import { initJsPsych } from 'jspsych';
 import { pressKey, startTimeline } from "@jspsych/test-utils";
+
+import { createTimeline } from "./experiment";
 
 jest.useFakeTimers();
 
 describe("createTimeline", () => {
+    // Create a div to hold the experiment in case experiment.js refers to it
+    document.body.innerHTML = '<div id="jsPsychTarget"></div>';
+    // Initialize jsPsych, since createTimeline takes jsPsych as an argument
+    let jsPsych = initJsPsych();
+    
     // This test should pass regardless of any user customizations of the template
     it("returns a valid jsPsych timeline", async () => {
-        const timeline = createTimeline();
+        const timeline = createTimeline(jsPsych);
         // Check that the timeline is an array
         expect(Array.isArray(timeline)).toBeTruthy();
         // Check that the timeline contains at least one trial
@@ -23,7 +30,7 @@ describe("createTimeline", () => {
     // This test will probably fail if the user customizes the template
     it("returns a timeline with a single 'Hello, world!' trial", async () => {
         // Check that the timeline runs and finishes with expected data
-        const timeline = createTimeline();
+        const timeline = createTimeline(jsPsych);
         const { expectFinished, getData } = await startTimeline(timeline);
         jest.advanceTimersByTime(1000);
         pressKey("a");
