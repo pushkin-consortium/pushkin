1cc1609fe5e67b2efc5854785f1fb860737836e0
jessestorbeck <jesse.storbeck@bc.edu>
2024-03-27 09:54:08 -0400

    Fixing failed WebSocket connections in Codespaces

--- .changeset/modern-pillows-boil.md
+++ .changeset/modern-pillows-boil.md
@@ -3,4 +3,6 @@
 "pushkin-cli": patch
 ---
 
-Added logic to front end's config.js to automatically detect use in GitHub Codespaces and appropriately set API endpoints
+- Added logic to front end's `config.js` to automatically detect use in GitHub Codespaces and appropriately set API endpoints
+- Similar logic added to front end's craco config to set WebSocket URL appropriately for Codespaces
+- Added detection of Codespaces-specific environment variables to CLI's `setEnv()` function
--- packages/pushkin-cli/src/commands/prep/index.js
+++ packages/pushkin-cli/src/commands/prep/index.js
@@ -108,15 +108,23 @@ const publishLocalPackage = async (modDir, modName, verbose) => {
   })
 };
 
+/**
+ * Creates/updates .env.js in the Pushkin site's pushkin/front-end/src directory
+ * @param {boolean} debug Whether the site is being prepped for testing (true) or deployment (false)
+ * @param {boolean} verbose Output extra information to the console for debugging purposes
+ */
 export function setEnv(debug, verbose) {
   if (verbose) {
     console.log('--verbose flag set inside setEnv()');
-    console.log('running setEnv()');
+    console.log('Updating pushkin/front-end/src/.env.js');
   }
   try {
-    let envJS = `export const debug = ${debug};
-      export const codespaces = ${process.env.CODESPACES};
-      export const codespaceName = '${process.env.CODESPACE_NAME}';`;
+    let envJS = `const debug = ${debug};
+      // The following will be undefined in the local environment
+      // but are needed to route the API and server correctly for GitHub Codespaces
+      const codespaces = ${process.env.CODESPACES};
+      const codespaceName = '${process.env.CODESPACE_NAME}';
+      module.exports = { debug, codespaces, codespaceName };`;
     envJS = envJS.split('\n').map((line) => line.trim()).join('\n'); // Remove indentation
     fs.writeFileSync(path.join(process.cwd(), 'pushkin/front-end/src', '.env.js'), envJS, 'utf8')
   } catch (e) {
