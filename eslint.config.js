d4d180d6dc939b04ac7c8218b172923e7be33be6
jessestorbeck <jesse.storbeck@bc.edu>
2024-04-09 16:58:16 -0400

    changes to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,13 @@
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const eslintJS = require("@eslint/js");
 
 module.exports = [
+  eslintJS.configs.recommended,
+  {
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
   // Any other config imports go at the top
   eslintPluginPrettierRecommended,
 ];
