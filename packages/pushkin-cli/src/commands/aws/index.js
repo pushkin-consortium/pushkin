7a8cbe01507d8bcee254569afd59986079133b35
Becky Gilbert <beckyannegilbert@gmail.com>
2026-02-26 16:21:48 -0800

    remove incorrect credentials param in cloudfront distribution config (cloudFrontClient handles auth)

--- packages/pushkin-cli/src/commands/aws/index.js
+++ packages/pushkin-cli/src/commands/aws/index.js
@@ -979,7 +979,6 @@ const deployFrontEnd = async (
       myCloud = await cloudFrontClient.send(
         new CreateDistributionWithTagsCommand({
           DistributionConfigWithTags: {
-            credentials: useIAM.iam,
             DistributionConfig: myCloudFront.DistributionConfig,
             Tags: {
               Items: myCloudFront.Tags.Items,
