9e517ea5259152042c58e6004bdf70a85d6e6bbb
cchang-vassar <79338042+cchang-vassar@users.noreply.github.com>
2026-04-21 14:52:06 -0700

    update docker.js utlility by removing previous duplicate

--- packages/pushkin-cli/src/utils/docker.js
+++ packages/pushkin-cli/src/utils/docker.js
@@ -1,41 +1,180 @@
-const dockerLogin = async () => {
-  //get dockerhub id
-  let DHID;
+import fs from "graceful-fs";
+import path from "path";
+import jsYaml from "js-yaml";
+import { quote } from "shell-quote";
+import { exec } from "../commands/aws/constants.js";
+import { readConfig } from "../commands/prep/index.js";
+import { loadPushkinConfig } from "./config.js";
+import { readFile } from "./file.js";
+
+/**
+ * Rebuilds the worker for a given experiment
+ * WHY: Workers need to be built for Linux/AMD64 in order to be deployed to AWS, and the CLI may be
+ * running on a different platform. By rebuilding the worker and pushing it to DockerHub, we can
+ * ensure that the correct image is available for AWS to pull
+ * @param {string} experiment – The experiment name for which to rebuild the worker
+ * @param {boolean} verbose – Whether to log details
+ * @returns {Promise} - A promise that resolves when the worker is rebuilt
+ */
+const rebuildWorker = async function (experiment, verbose = false) {
+  let pushkinConfig;
   try {
-    let config = await loadConfig(path.join(process.cwd(), "pushkin.yaml"));
-    DHID = config.DockerHubID;
-  } catch (e) {
-    console.error(`Unable to load pushkin.yaml`);
-    throw e;
+    pushkinConfig = await loadPushkinConfig();
+  } catch (error) {
+    console.error(`Failed to load pushkin.yaml:`, error);
+    throw error;
   }
-
-  if (DHID == "") {
-    throw new Error(`Your DockerHub ID has disappeared from pushkin.yaml.\n I am not sure how that happened.\n
-      If you run '$ pushkin setDockerHub' and then retry aws update, it might work. Depending on exactly why your DockerHub ID wasn't in pushkin.yaml.`);
+  if (verbose) {
+    console.log(`Rebuilding AWS-compatible worker for ${experiment}`);
   }
-
+  const expDir = path.join(process.cwd(), pushkinConfig.experimentsDir, experiment);
   try {
-    console.log(`Confirming docker login.`);
-    execSync(`cat .docker | docker login --username ${DHID} --password-stdin`);
-  } catch (e) {
-    console.error(`Automatic login to DockerHub failed. This might be because your ID or password are wrong.\n
-      Try running '$ pushkin setDockerHub' and reset then try again.\n
-      If that still fails, report an issue to Pushkin on GitHub. In the meantime, you can probably login manually\n
-      by typing '$ docker login' into the console.\n Provide your username and password when asked.\n
-      Then try '$ pushkin aws update' again.`);
-    process.exit();
+    if (!fs.lstatSync(expDir).isDirectory()) return "";
+  } catch (error) {
+    console.warn(`Experiment directory not found: ${expDir}`, error);
+    return "";
   }
+  let expConfig;
+  try {
+    expConfig = readConfig(expDir);
+  } catch (error) {
+    console.error(`Failed to read experiment config file for ${experiment}:`, error);
+    throw error;
+  }
+  const workerConfig = expConfig.worker;
+  const workerName = `${experiment.toLowerCase()}_worker`; // Docker names must all be lower case
+  const workerLoc = path.join(expDir, workerConfig.location);
 
-  return DHID;
+  let workerBuild;
+  try {
+    const command = [
+      "docker",
+      "buildx",
+      "build",
+      "--platform",
+      "linux/amd64",
+      workerLoc,
+      "-t",
+      workerName,
+      "--load",
+    ];
+    workerBuild = exec(quote(command));
+  } catch (error) {
+    console.error(`Problem building worker for ${experiment}:`, error);
+    throw error;
+  }
+  return workerBuild;
 };
 
-const updateDocker = async () => {
-  let DHID = await dockerLogin();
-
-  try {
-    return publishToDocker(DHID);
-  } catch (e) {
-    console.error("Unable to publish images to DockerHub");
-    throw e;
+/**
+ * Publish Docker images to DockerHub:
+ * 1. Pushkin API
+ * 2. Experiment workers
+ * WHY: This is necessary for deploying workers to AWS, since they need to be built for Linux/AMD64
+ * and the CLI may be running on a different platform. By pushing to DockerHub, we can ensure that
+ * the correct images are available for AWS to pull
+ * @param {string} DHID - The DockerHub ID
+ * @param {Promise} rebuiltWorkers - Optional promise to wait for workers that need to be rebuilt
+ * to finish rebuilding before pushing. The function itself pushes all workers on docker-compose
+ * @param {boolean} verbose – Whether to log detailed steps
+ * @returns {Promise} - A promise that resolves when the images are published
+ */
+const publishToDocker = async (DHID, rebuiltWorkers = Promise.resolve(), verbose = false) => {
+  if (!DHID) {
+    throw new Error(
+      "DockerHub ID is required to publish images to Docker. Please set it using '$ pushkin setDockerHub' and try again.",
+    );
   }
+  console.log("Publishing images to DockerHub");
+
+  // Build API image
+  if (verbose) {
+    console.log("Building API");
+  }
+  try {
+    const imageTag = `${DHID}/api:latest`;
+    const buildCommand = quote([
+      "docker",
+      "buildx",
+      "build",
+      "--platform",
+      "linux/amd64",
+      "-t",
+      imageTag,
+      "pushkin/api",
+      "--load",
+    ]);
+    await exec(buildCommand, { cwd: process.cwd() });
+  } catch (error) {
+    console.error(`Problem building API:`, error);
+    throw error;
+  }
+
+  // Push API image to DockerHub
+  if (verbose) {
+    console.log("Pushing API to DockerHub");
+  }
+  let pushedAPI;
+  try {
+    const imageTag = `${DHID}/api:latest`;
+    const pushCommand = quote(["docker", "push", imageTag]);
+    pushedAPI = exec(pushCommand, { cwd: process.cwd() }); // async; awaited in Promise.all at the end
+  } catch (error) {
+    console.error(`Couldn't push API to DockerHub:`, error);
+    throw error;
+  }
+  // NOTE: don't need to build frontend because it's all compiled static files
+
+  let docker_compose;
+  try {
+    const dockerComposePath = path.join(process.cwd(), "pushkin/docker-compose.dev.yml");
+    docker_compose = jsYaml.load(readFile(dockerComposePath, "utf8"));
+  } catch (error) {
+    console.error(`Failed to load the docker-compose:`, error);
+    throw error;
+  }
+
+  // Push worker images to DockerHub
+  // WHY: AWS ECS needs to pull worker images from a registry (DockerHub), not from local
+  const pushWorkers = async (s) => {
+    const service = docker_compose.services[s];
+    if (service.labels == null) {
+      // not a worker
+      return "";
+    }
+    if (service.labels.isPushkinWorker != true) {
+      // not a worker
+      return "";
+    }
+
+    if (verbose) {
+      console.log(`Pushing service: ${s}`);
+    }
+    try {
+      const imageName = service.image.split(":")[0];
+      const targetImage = `${DHID}/${imageName}:latest`;
+      const tagCommand = quote(["docker", "tag", service.image, targetImage]);
+      const pushCommand = quote(["docker", "push", targetImage]);
+      await exec(tagCommand);
+      return exec(pushCommand); // async; awaited in Promise.all at the end
+    } catch (error) {
+      console.error(`Unable to tag and/or push image ${service.image}:`, error);
+      throw error;
+    }
+  };
+
+  await rebuiltWorkers; // Can't push workers until they finish building
+
+  let pushedWorkers;
+  try {
+    pushedWorkers = Object.keys(docker_compose.services).map(pushWorkers);
+  } catch (error) {
+    console.error(`Unable to push worker images to DockerHub:`, error);
+    throw error;
+  }
+
+  return Promise.all([pushedAPI].concat(pushedWorkers));
 };
+
+// Export functions
+export { rebuildWorker, publishToDocker };
