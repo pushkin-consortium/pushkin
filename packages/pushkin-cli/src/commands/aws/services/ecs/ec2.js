7a81c9ce3f201ebda60effe29cda289a5a642db5
cchang-vassar <79338042+cchang-vassar@users.noreply.github.com>
2026-05-28 01:27:51 -0400

    move VPC and subnets logic into ecs/ec2.js

---
+++ packages/pushkin-cli/src/commands/aws/services/ecs/ec2.js
@@ -0,0 +1,50 @@
+/**
+ *
+ * @module aws/services/ecs/ec2
+ */
+
+import { EC2Client, DescribeSubnetsCommand, DescribeVpcsCommand } from "@aws-sdk/client-ec2";
+import { AWSClientFactory } from "../utils/aws-client-factory.js";
+import { AWS_REGION } from "../constants.js";
+
+function createEc2Client(awsProfileName) {
+  const factory = new AWSClientFactory(AWS_REGION, awsProfileName);
+  return factory.createClient(EC2Client);
+}
+
+/**
+ * Get the default VPC (Virtual Private Cloud) ID for the current region.
+ * WHY: VPC is needed for both the load balancer and Fargate tasks to define the network environment they run in.
+ * @param {string} awsProfileName - IAM profile name
+ * @returns {Promise<string>} Default VPC ID
+ */
+async function getDefaultVpc(awsProfileName) {
+  const ec2Client = createEc2Client(awsProfileName);
+  const { Vpcs } = await ec2Client.send(new DescribeVpcsCommand({}));
+  const defaultVpc = Vpcs.find((v) => v.IsDefault);
+  if (!defaultVpc)
+    throw new Error(
+      "No default VPC found in region. To restore it, see https://docs.aws.amazon.com/vpc/latest/userguide/default-vpc.html#create-default-vpc",
+    );
+  return defaultVpc.VpcId;
+}
+
+/**
+ * Asynchronously retrieves available subnets (sets of IP addresses) in the AWS zone and maps them by availability zone.
+ * WHY: Subnets are needed for both the load balancer and Fargate tasks to define the network environment they run in.
+ * @param {string} awsProfileName - AWS IAM profile name to use for authentication
+ * @returns {Promise} A promise that resolves to an object mapping availability zones to subnet IDs
+ */
+async function getSubnets(awsProfileName) {
+  const ec2Client = createEc2Client(awsProfileName);
+  const { Subnets } = await ec2Client.send(new DescribeSubnetsCommand({}));
+  const subnets = Subnets.reduce((acc, subnet) => {
+    // This overwrites any duplicate availability zones (multiple subnets in the same zone) with
+    // the last one found, but in practice, AWS should not return duplicates.
+    acc[subnet.AvailabilityZone] = subnet.SubnetId;
+    return acc;
+  }, {});
+  return subnets;
+}
+
+export { getSubnets, getDefaultVpc };
